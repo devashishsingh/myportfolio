eab510585c6848f80eb4b51f8b823d9d8b2f7a1d
devashishsingh <devashish.singh12@gmail.com>
2026-04-07 02:14:51 +0800

    feat: add Google Analytics (G-RBJJRE06QR)

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,10 @@ export default function RootLayout({children}:{children:React.ReactNode}){
         <Script id="microsoft-clarity" strategy="beforeInteractive">
           {`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","w7et9jfxj4");`}
         </Script>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-RBJJRE06QR" strategy="beforeInteractive" />
+        <Script id="google-analytics" strategy="beforeInteractive">
+          {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-RBJJRE06QR');`}
+        </Script>
       </head>
       <body>
         <Header />
